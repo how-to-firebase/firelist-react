6dc92efe7e15d759a50be745dd91a50cf8dc847c
Chris Esplin <chris-esplin@pluralsight.com>
2018-04-30 14:43:11 +0000

    Clean up styles. Add delete svg.

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,6 +1,7 @@
 export AccountBox from './account-box';
 export Bolt from './bolt';
 export Dashboard from './dashboard';
+export Delete from './delete';
 export Exit from './exit';
 export NoteAdd from './note-add';
 export Person from './person';
