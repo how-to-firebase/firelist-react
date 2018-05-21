9f63dcb001c758be439333a172296200daeaa589
Chris Esplin <chris-esplin@pluralsight.com>
2018-05-21 12:13:25 +0000

    Preparing for deploy

---
+++ bin/copy-public.sh
@@ -0,0 +1,6 @@
+cp -R public/assets dist/assets 
+cp -R public/environments dist/environments 
+cp public/manifest.json dist/manifest.json 
+cp public/style.css dist/style.css 
+cp public/sw.js dist/sw.js
+
