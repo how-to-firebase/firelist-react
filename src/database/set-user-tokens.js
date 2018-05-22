3d9bfa43f8d4bc2369eaa8aba17137aa8cf86b55
Chris Esplin <chris@christopheresplin.com>
2018-05-22 07:17:05 -0600

    Clarify Realtime DB instructions

--- src/database/set-user-tokens.js
+++ src/database/set-user-tokens.js
@@ -1,30 +1,29 @@
 /* globals firebase environment */
 import { omitEmptyValues } from '../utilities';
 
-/* 
-  CHALLENGE Realtime DB
-  - See 
-  - Create a reference to notesCollection
-  - Hint: It's just a collection under the name 'notes'
-*/
 const db = firebase.database();
 
 export default async ({ environment, messagingToken, currentUser }) => {
   const { uid } = currentUser;
   const idToken = await currentUser.getIdToken()
+  
+  /* 
+    CHALLENGE Realtime DB
+    - See https://firebase.google.com/docs/database/web/start
+    - Create a userTokensRef with the path 
+      `${environment.firebaseRoot}/userWriteable/userTokens/${currentUser.uid}`
+    - Create a userTokens variable with the data shape of { idToken, messagingToken }
+    - Use omitEmptyValues({ idToken, messagingToken }) to clear out undefined, null or '' values
+    - Set userTokensRef to the new userTokens object
+    - Hint: Firebase does not like null values :)
+      Keep your data "sparse"... don't try to save empty data.
+  */
+
   const userTokensRef = db
     .ref(environment.firebaseRoot)
     .child('userWriteable/userTokens')
     .child(currentUser.uid);
 
-  /* 
-    CHALLENGE Realtime DB
-    - See 
-    - Use omitEmptyValues(note) to clear out undefined, null or '' values
-    - Add the note to notesCollection with the .add(cleanNote) function
-    - Hint: While Firestore supports null values, it does not support undefined values
-    -       Also, it's generally preferred to store nothing at all instead of a null
-  */
   const userTokens = omitEmptyValues({
     idToken,
     messagingToken,
