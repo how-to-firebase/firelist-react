9fcd20db57d38537cacc5705a6538aff0eb651ca
Chris Esplin <chris@christopheresplin.com>
2018-05-25 07:35:37 -0600

    Adding another stop to the layout.js auth prompt

--- src/layout.js
+++ src/layout.js
@@ -30,10 +30,11 @@ export class InnerApp extends React.Component {
       - https://firebase.google.com/docs/auth/web/manage-users
       - Register an onAuthStateChanged callback
       - Call setCurrentUser with the updated currentUser
+      - If a currentUser exists, call setUserTokens({ environment, currentUser})
     */
-   
+
     const { environment, setCurrentUser } = this.props;
-    
+
   }
 
   render() {
