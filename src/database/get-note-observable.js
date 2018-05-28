684f3d958c7377d53efed14d6e77caed4c17c706
Chris Esplin <chris@christopheresplin.com>
2018-05-28 08:13:57 -0600

    Repairing get-note-observable prompt

--- src/database/get-note-observable.js
+++ src/database/get-note-observable.js
@@ -16,13 +16,14 @@ export default noteId => {
     - Create a reference to the doc specified by noteId
   */
   const docRef = notesCollection.doc(noteId);
+
   return Observable.create(observer => {
     /* 
       CHALLENGE Firestore 09
       - See https://firebase.google.com/docs/firestore/query-data/listen
       - See http://reactivex.io/rxjs/manual/overview.html#observable
-      - Listen to notesCollection with onSnapshot
-      - Pass doc into formatDoc() to format the doc to a more useful data shape
+      - Listen to docRef with onSnapshot
+      - Pass the resulting doc into formatDoc() to format the doc to a more useful data shape
       - Call observer.next with the result of formatDoc
       - Hint: Make sure to return onSnapshot's unsubscribe function from this function.
               RxJs takes whatever function you return from Observable.create and uses it as the
